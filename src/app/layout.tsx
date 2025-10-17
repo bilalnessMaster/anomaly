fa914397897498e8ee2ddf71f86e8d2cbc0bef63
bilalnessmaster <jijahgiga@gmail.com>
2025-10-17 10:39:19 +0100

    hide the navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div>
+          <div>
+{/*
+
           <Navbar />
+*/}
         </div>
         <div>
           {children}
